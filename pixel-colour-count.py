5805a0444f815ea94f556a17e838c510ebed0c3d
james <j.alsop99@hotmail.com>
2021-10-14 16:12:13 +0100

    new clustering algorithm

--- pixel-colour-count.py
+++ pixel-colour-count.py
@@ -58,7 +58,7 @@ def main():
     i = 0
     j = i + 1
 
-    for color, count in sorted_counts.items():
+    for rgb_1_value, rgb_1_count in sorted_counts.items():
 
         # Check to avoid StopIteration
 
@@ -67,51 +67,50 @@ def main():
 
         standalone_color = True
 
-        rgb_1_value = color
-        rgb_1_count = count
+        matched_color = None
+
         color_1_rgb = sRGBColor(rgb_1_value[0],rgb_1_value[1],rgb_1_value[2], is_upscaled=True)
         color_1_lab = convert_color(color_1_rgb, LabColor)
 
-        for color, count in sorted_counts.items():
+        for rgb_2_value, rgb_2_count in average_colors_dict.items():
 
-            rgb_2_value = next(islice(sorted_counts.keys(), j, None))
-            rgb_2_count = next(islice(sorted_counts.values(), j, None))
             color_2_rgb = sRGBColor(rgb_2_value[0],rgb_2_value[1],rgb_2_value[2], is_upscaled=True)
             color_2_lab = convert_color(color_2_rgb, LabColor)
 
             delta_e = delta_e_cie2000(color_1_lab, color_2_lab)
-            #print('{}:{}:{}:{}:{}'.format(i, j, delta_e, rgb_1_value, rgb_2_value))
 
             if delta_e < 30:
                 
                 standalone_color = False
 
-                # Calculate weighted RGB value
+                matched_color = rgb_2_value
 
-                combined_pixel_count = rgb_1_count + rgb_2_count
-                
-                average_r = (rgb_1_value[0]*rgb_1_count + rgb_2_value[0]*rgb_2_count ) / combined_pixel_count
-                average_g = (rgb_1_value[1]*rgb_1_count + rgb_2_value[1]*rgb_2_count ) / combined_pixel_count
-                average_b = (rgb_1_value[2]*rgb_1_count + rgb_2_value[2]*rgb_2_count ) / combined_pixel_count
-
-                rgb_1_value = (average_r, average_g, average_b)
-                rgb_1_count = combined_pixel_count
-                average_colors_dict[rgb_1_value] = combined_pixel_count
-            
-            # Amend index, with check to avoid StopIteration
-
-            if j == len(sorted_counts) - 1:
-                i = i + 1
-                j = i + 1
                 break
-            else:
-                j += 1
+
 
         if standalone_color == True:
 
             average_colors_dict[rgb_1_value] = rgb_1_count
 
-    #print(average_colors_dict)
+        else:
+            # Calculate weighted RGB value
+
+            rgb_2_value = matched_color
+            rgb_2_count = average_colors_dict[matched_color]
+
+            combined_pixel_count = rgb_2_count + rgb_1_count 
+                
+            average_r = (rgb_1_value[0]*rgb_1_count + rgb_2_value[0]*rgb_2_count ) / combined_pixel_count
+            average_g = (rgb_1_value[1]*rgb_1_count + rgb_2_value[1]*rgb_2_count ) / combined_pixel_count
+            average_b = (rgb_1_value[2]*rgb_1_count + rgb_2_value[2]*rgb_2_count ) / combined_pixel_count
+
+            rgb_average = (average_r, average_g, average_b)
+
+            del average_colors_dict[matched_color]
+            average_colors_dict[rgb_average] = combined_pixel_count
+        
+
+    # print("colours", average_colors_dict)
 
     for color, count in average_colors_dict.items():
 
