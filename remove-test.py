98daa6f89fa6319ecb31f47c82f9118ac3f0e768
james <j.alsop99@hotmail.com>
2021-10-18 17:18:41 +0100

    testing rembg package

---
+++ remove-test.py
@@ -0,0 +1,13 @@
+
+from rembg.bg import remove
+import numpy as np
+import io
+from PIL import Image
+
+input_path = r".\test_images\sample2.png"
+output_path = 'out.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
